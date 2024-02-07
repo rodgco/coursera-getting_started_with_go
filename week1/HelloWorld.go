61a6e8ed20ae8b352bad72b77592690771ec8dea
Rodrigo Couto <r@rodg.co>
2024-02-07 08:48:09 -0300

    Week1 Asessment

---
+++ week1/HelloWorld.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// As part of Coursera "Getting Started with Go" course - Week 1
+func main() {
+	fmt.Println("Hello, world!")
+}
---
+++ week1/go.mod
@@ -0,0 +1,3 @@
+module rodgco/coursera-getting_started_with_go/HelloWorld
+
+go 1.21.6
